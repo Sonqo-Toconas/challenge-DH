022c7da9084888f7930b1aa62dbedaa369dd3d2d
Sonqo-Toconas <sonqo_tst@hotmail.com>
2024-01-30 21:39:51 -0300

    Primer Commit

---
+++ back-end/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+const apiRoutes = require('./src/routes/apiRoutes');
+
+app.use('/api', apiRoutes);
+
+app.listen(3030, () => {
+    console.log('Servidor corriendo en el puerto http://localhost:3030');
+});
